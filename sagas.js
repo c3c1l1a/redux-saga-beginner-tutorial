83a7b8a50cf6ab0aa2a966b75988eb6b3f8867be
c3c1l1a <cesswangumukima@gmail.com>
2023-03-27 16:41:30 +0800

    Added a button that increments counter by 1 but with a delay of 1 second

--- sagas.js
+++ sagas.js
@@ -1,3 +1,24 @@
+import { put, takeEvery, all } from 'redux-saga/effects'
+
+const delay = (ms) => new Promise(res => setTimeout(res, ms))
+
 export function* helloSaga(){
 	console.log('Hello Saga!');
 }
+
+export function* increamentAsync(){
+	yield delay(1000)
+	yield put({ type: 'INCREMENT' })
+}
+
+export function* watchIncreamentAsync(){
+	yield takeEvery('INCREMENT_ASYNC', increamentAsync)
+}
+
+export function* rootSaga(){
+	yield all([
+		helloSaga(),
+		watchIncreamentAsync()
+	])
+}
+
